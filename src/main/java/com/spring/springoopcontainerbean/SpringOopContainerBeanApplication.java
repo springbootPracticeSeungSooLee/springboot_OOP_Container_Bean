f834f5c1cafbed7d9cb9bc4d830cafde206bdcf1
leeseungsoo <seungsoo0701@gmail.com>
2022-03-02 02:44:30 +0900

    프로젝트 세팅

---
+++ src/main/java/com/spring/springoopcontainerbean/SpringOopContainerBeanApplication.java
@@ -0,0 +1,13 @@
+package com.spring.springoopcontainerbean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOopContainerBeanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOopContainerBeanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/springoopcontainerbean/SpringOopContainerBeanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springoopcontainerbean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOopContainerBeanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
